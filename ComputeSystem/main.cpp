40f8dcd80917979044fa84be5f1920157ad73071
Riccardo <richi.franchi2004@gmail.com>
2021-04-01 08:59:50 +0200

    Set fullscreen

--- ComputeSystem/main.cpp
+++ ComputeSystem/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	Window window = Window(1920, 1080, "Physarum Simulation", 0, false);
+	Window window = Window(1920, 1080, "Physarum Simulation", 0, true);
 	window.run();
 
 	return 0;
